790346c045d1b8f975d95628e7d47e21406727fa
Thainara Onofre <thainaraonofre@outlook.com>
2023-06-13 18:19:55 -0300

    Enviado topicos basicos do Java

---
+++ topicosBasicos/src/topicosbasicos/Bitwise.java
@@ -0,0 +1,24 @@
+package topicosbasicos;
+
+import java.util.Scanner;
+
+public class Bitwise {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int mask = 32;
+		int n = sc.nextInt();
+		
+		if ((n & mask) != 0) {
+			System.out.println("6th bit is true");
+		}
+		else {
+			System.out.println("6th bit is false");
+		}
+		
+sc.close();	
+		
+	}
+
+}
